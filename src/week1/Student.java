acff140c469371b89c4a3cc5dcbdcfd9083b042c
doxuandung1999 <dungrma99@gmail.com>
2018-09-18 22:38:28 +0700

    nop bai

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -3,24 +3,55 @@ package week1;
 public class Student {
 
     // TODO: khai báo các thuộc tính cho Student
-
+    private String name;
+    private String id;
+    private String group;
+    private String email;
     // TODO: khai báo các phương thức getter, setter cho Student
-
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getGroup() {
+        return group;
+    }
+    public void setGroup(String group) {
+        this.group = group;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
     /**
      * Constructor 1
      */
     Student() {
         // TODO:
+        this.name = "Student";
+        this.id = "000";
+        this.email = "uet@vnu.edu.vn";
+        this.group = "INT 22041";
     }
 
     /**
      * Constructor 2
-     * @param n
-     * @param sid
-     * @param em
      */
     Student(String n, String sid, String em) {
         // TODO:
+        this.name = n;
+        this.id = sid;
+        this.email = em;
+        this.group = "INT 22042";
     }
 
     /**
@@ -29,10 +60,18 @@ public class Student {
      */
     Student(Student s) {
         // TODO:
+        this.name = s.name;
+        this.id = s.id;
+        this.email = s.email;
+        this.group = s.group;
     }
 
     String getInfo() {
         // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+        return this.getName() + "\n"
+                + this.getId() + "\n"
+                + this.getGroup() + "\n"
+                + this.getEmail();
     }
 }
+
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -3,21 +3,81 @@ package week1;
 public class StudentManagement {
 
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
+    Student students[] = new Student[100];
 
-    public boolean sameGroup(Student s1, Student s2) {
+    public  boolean sameGroup(Student s1, Student s2) {
         // TODO:
-        return false; // xóa dòng này sau khi cài đặt
+        return s1.getGroup()==s2.getGroup();
     }
 
     void studentsByGroup() {
         // TODO:
+        int n = students.length;
+        for(int i=0;i<n;i++)
+        {
+            if(this.students[i].getGroup().equals("INT 22041"))
+            {
+                System.out.println(this.students[i].getInfo());
+            }
+            else if(this.students[i].getGroup().equals("INT 22042"))
+            {
+                System.out.println(this.students[i].getInfo());
+            }
+        }
     }
 
     void removeStudent(String id) {
         // TODO:
+        int vitri = 0 , n = students.length;
+        for(int i=0;i<n;i++)
+        {
+            if(this.students[i].getId().equals(id))
+            {
+                vitri = i;
+                break;
+            }
+        }
+        for(int i = vitri ; i<n-1;i++)
+        {
+            this.students[i] = this.students[i+1];
+        }
+        this.students[n-1] = null;
+        n--;
     }
 
     public static void main(String[] args) {
         // TODO:
+        Student hocSinh1 = new Student();
+
+	Student hocSinh2 = new Student("do" , "555" , "do@vnu.edu.vn");
+
+	Student hocSinh3 = new Student(hocSinh2);
+
+     StudentManagement st = new StudentManagement();
+
+    st.students[0]= new Student();
+     st.students[0].setName("dung");
+     st.students[0].setId("111");
+     st.students[0].setGroup("INT 22041");
+     st.students[0].setEmail("dung@vnu.edu.vn");
+
+    st.students[1] = new Student();
+     st.students[1].setName("do dung");
+     st.students[1].setId("222");
+     st.students[1].setGroup("INT 22041");
+     st.students[1].setEmail("dung@vnu.edu.vn");
+
+     st.students[2] = new Student();
+     st.students[2].setName("xuan dung");
+     st.students[2].setId("333");
+     st.students[2].setGroup("INT 22042");
+     st.students[2].setEmail("dung@vnu.edu.vn");
+
+     System.out.println(st.sameGroup(hocSinh1,hocSinh2) + "\n"+ st.sameGroup(hocSinh2,hocSinh3));
+     st.removeStudent("333");
+     System.out.println("danh sach luc sau : " + "\n");
+     st.studentsByGroup();
+
     }
+
 }
