c83eb408a06128051c3b23fee3799d19d76202cb
doxuandung1999 <dungrma99@gmail.com>
2018-10-02 23:04:15 +0700

    nop bai lai

--- test/week3/Week3Test.java
+++ test/week3/Week3Test.java
@@ -16,16 +16,16 @@ public class Week3Test {
         Assert.assertEquals(345, Week3.max(215 ,345));
 
         // TODO: Viết 5 testcase cho phương thức minOfArray()
-            int arr_1[] = new int[] {4 , 3 ,5 };
-            int arr_2[] = new int[] {6 , 3 , 7 , 0 , 12 , 6};
-            int arr_3[] = new int[] {122 , 99 , 67 , 40 , 12 , 5};
-            int arr_4[] = new int[] {1 , 3 , 5 , 0 , 12 };
-            int arr_5[] = new int[] {6 , 13 , 8 , 10 , 2 , 7};
-
+            
+        int arr_1[] = new int[] {4 , 3 ,5 };
             Assert.assertEquals(3 ,Week3.minOfArray(arr_1));
+        int arr_2[] = new int[] {6 , 3 , 7 , 0 , 12 , 6};
             Assert.assertEquals(0 , Week3.minOfArray(arr_2));
+        int arr_3[] = new int[] {122 , 99 , 67 , 40 , 12 , 5};
             Assert.assertEquals(5 ,Week3.minOfArray(arr_3));
+        int arr_4[] = new int[] {1 , 3 , 5 , 0 , 12 };
             Assert.assertEquals(0 , Week3.minOfArray(arr_4));
+        int arr_5[] = new int[] {6 , 13 , 8 , 10 , 2 , 7};
             Assert.assertEquals(2 , Week3.minOfArray(arr_5));
 
         // TODO: Viết 5 testcase cho phương thức calculateBMI()
